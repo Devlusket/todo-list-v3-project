39b077abcdbdbd3a418d64dd44fbd03e73fa97ad
Lucas Soares <tsuunakage@gmail.com>
2025-08-28 16:04:19 -0300

    fix page name

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head>
+      <title>App de Metas</title>
+    </Head>
+      <Component {...pageProps} />
+    </>
+  )
+    
+    
 }
